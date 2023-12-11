b036f4c9728870b9fcffeeef575599bdd56230da
sjyoung428 <sjy8267@naver.com>
2023-12-11 17:35:17 +0900

    home

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,8 +4,10 @@ package hello.hellospring.repository;
 
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
+
 
 public class MemoryMemberRepository implements MemberRepository {
 
